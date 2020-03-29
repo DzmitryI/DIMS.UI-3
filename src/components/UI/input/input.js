ac3019ba233cbd79ab6d9fc617f8ac9cdd33fb21
Dzmitry Ivanou <3prn@mail.ru>
2020-03-29 18:38:44 +0300

    fix: bug in input.js

--- src/components/UI/input/input.js
+++ src/components/UI/input/input.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+function isInvalid(valid, touched, shouldValidation) {
+  return !valid && touched && shouldValidation;
+}
 const Input = ({
   type = 'text',
   autocomplete = 'on',
@@ -13,14 +15,12 @@ const Input = ({
   errorMessage,
   disabled = false,
 }) => {
-  function isInvalid(valid, touched, shouldValidation) {
-    return !valid && touched && shouldValidation;
-  }
+  const result = isInvalid(valid, touched, shouldValidation);
   return (
-    <div className={isInvalid(valid, touched, shouldValidation) ? `form-group invalid` : `form-group`}>
+    <div className={result ? `form-group invalid` : `form-group`}>
       <label htmlFor={id}>{label}</label>
       <input type={type} id={id} value={value} onChange={onChange} disabled={disabled} autoComplete={autocomplete} />
-      {isInvalid(valid, touched, shouldValidation) ? <span>{errorMessage}</span> : null}
+      {result ? <span>{errorMessage}</span> : null}
     </div>
   );
 };
