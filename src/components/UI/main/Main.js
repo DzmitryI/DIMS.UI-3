8a6f5e383b294f0fb17427aa0722790e86486eff
Dzmitry Ivanou <3prn@mail.ru>
2020-04-18 16:00:47 +0300

    feat: finished main page

--- src/components/UI/main/Main.js
+++ src/components/UI/main/Main.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import Footer from '../footer';
-import humanImg from '../../../assets/images/human.png';
+import imgHuman from '../../../assets/images/human.png';
+import imgMail from '../../../assets/images/mail.png';
+import imgGit from '../../../assets/images/git.png';
+import { withTheme } from '../../../hoc';
 
-const Main = () => {
+const Main = ({ theme }) => {
   return (
     <div className='main-page__wrap'>
       <h2>Welcome to DIMS</h2>
       <div className='dims__inner'>
         <div className='dims'>
-          <img src={humanImg} alt='human' />
+          <img src={imgHuman} alt='human' />
         </div>
+      </div>
+      <div className={`dims-develop__wrap dims-develop__wrap--${theme}`}>
+        <p>App developed by Ivanov Dmitriy</p>
+        <a href='mailto:dmitriy.ivanou@gmail.com' target='_blank'>
+          <img src={imgMail} width='32px' alt='mail' />
+        </a>
+        <a href='https://github.com/DzmitryI' target='_blank'>
+          <img src={imgGit} width='32px' alt='git' />
+        </a>
       </div>
       <Footer />
     </div>
   );
 };
 
-export default Main;
+export default withTheme(Main);
