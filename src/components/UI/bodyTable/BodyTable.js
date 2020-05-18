27b5fce596489f5403b6266045700d864f57115b
Dzmitry Ivanou <3prn@mail.ru>
2020-05-19 00:07:33 +0300

    fix: notes from PR

--- src/components/UI/bodyTable/BodyTable.js
+++ src/components/UI/bodyTable/BodyTable.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const BodyTable = ({ arr }) =>
-  arr.map((arrTd, index) => {
+  arr.map((arrTd) => {
     const renderItems = (bodies) =>
       bodies.map(({ className, value }, index) => (
         <td key={className + index} className={className}>
           {value}
         </td>
       ));
-    return <tr key={arrTd + index}>{renderItems(arrTd)}</tr>;
+    return <tr key={arrTd}>{renderItems(arrTd)}</tr>;
   });
 
 BodyTable.propTypes = {
