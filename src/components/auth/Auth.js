5ecdf4e597cffe68adf1b5e3b3e263b70f852d32
Dzmitry Ivanou <3prn@mail.ru>
2020-05-18 19:40:24 +0300

    fix: notes from PR

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -7,6 +7,7 @@ import Button from '../UI/button';
 import Radio from '../UI/radio';
 import { createControl, validateControl } from '../../services/helpers';
 import { auth } from '../../store/actions/auth';
+import imgLogo from '../../assets/images/logo.png';
 
 class Auth extends PureComponent {
   state = {
@@ -98,7 +99,8 @@ class Auth extends PureComponent {
       <>
         {onNotification && <DisplayNotification notification={notification} />}
         <div className='auth'>
-          <h1>Authorization</h1>
+          {/* <h1>Authorization</h1> */}
+          <img src={imgLogo} with='100px' height='35px' alt='logo' />
           <form onSubmit={this.submitHandler}>
             {this.renderInputs()}
             <fieldset className='base-wrap'>
